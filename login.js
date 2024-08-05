cd599eb3ef39c94a7cf2364b6c5ba208f563bcf2
Wasim Jafar <wasim.jafar_2026@ucentralasia.org>
2024-08-05 10:09:18 +0600

    third commit

---
+++ login.js
@@ -0,0 +1,12 @@
+document.getElementById('show-signup').addEventListener('click', function() {
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('signup-form').classList.remove('hidden');
+});
+
+document.getElementById('signin-button').addEventListener('click', function() {
+    // Add your login functionality here
+});
+
+document.getElementById('get-started-button').addEventListener('click', function() {
+    // Add your signup functionality here
+});
